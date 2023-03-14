083ce6a55fe45cae255d7cb6793d7f2c3ef8388b
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-03-15 01:41:18 +0300

    Completed assignment

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+    # Get /birds
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+    # Get /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "Not a Bird!"}, status: :not_found
+        end
+    end
+end
